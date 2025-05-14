a9035e951b5055fe5a84ae9ad7cb554e92f7fa30
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-05-14 21:40:44 +0200

    Make Map return slice only once

--- map.go
+++ map.go
@@ -3,8 +3,7 @@ package main
 // Map applies the function f on each element t in ts, transforming each t
 // from type T to type U, returning a new list of Us.
 func Map[T any, U any](ts []T, f func(t T) U) []U {
-	var us = make([]U, 0)
-	us = make([]U, 0, len(ts))
+	var us = make([]U, 0, len(ts))
 	for _, t := range ts {
 		us = append(us, f(t))
 	}
