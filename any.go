88d9caef5bf916b89fe0c688a8dbe324d913d24c
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-05-15 20:47:01 +0200

    Add Any function

---
+++ any.go
@@ -0,0 +1,12 @@
+package main
+
+// Any checks if any element in the slice satisfies the provided predicate function.
+// It returns true if at least one element meets the condition, otherwise false.
+func Any[T any](ts []T, predicate func(T) bool) bool {
+	for _, t := range ts {
+		if predicate(t) {
+			return true
+		}
+	}
+	return false
+}
