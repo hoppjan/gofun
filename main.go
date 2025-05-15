dce4b1738aaad8d78af45f9e03b2620746fc3f18
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-05-15 21:05:05 +0200

    Make main.go use all the new functions

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ func main() {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	numbers = Map(numbers, func(n int) int { return n * 10 })
 	fmt.Println(numbers)
-	reduced := Reduce(numbers, 0, func(n int, acc int) int { return n + acc })
+	filtered := Filter(numbers, func(n int) bool { return n%2 == 0 })
+	fmt.Println(filtered)
+	reduced := Reduce(filtered, 0, func(n int, acc int) int { return n + acc })
 	fmt.Println(reduced)
+	fmt.Println(*Find(numbers, 10))
+	fmt.Println(*FindMatch(numbers, func(n int) bool { return n%2 == 0 }))
+	fmt.Println(Any(numbers, func(n int) bool { return n == 5 }))
+	fmt.Println(All(numbers, func(n int) bool { return n%2 == 0 }))
+	fmt.Println(None(numbers, func(n int) bool { return n%2 == 0 }))
 }
